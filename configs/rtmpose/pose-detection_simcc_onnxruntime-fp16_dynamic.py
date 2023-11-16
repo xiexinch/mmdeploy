17593c1fe980bd899b3be8f2e5d65031a32c5e63
xiexinch <xiexinch@outlook.com>
2023-11-16 16:05:03 +0800

    --config=add rtmpose config

---
+++ configs/rtmpose/pose-detection_simcc_onnxruntime-fp16_dynamic.py
@@ -0,0 +1,21 @@
+_base_ = [
+    './pose-detection_static.py', '../_base_/backends/onnxruntime-fp16.py'
+]
+
+onnx_config = dict(
+    input_shape=[192, 256],
+    output_names=['simcc_x', 'simcc_y', 'simcc_z'],
+    dynamic_axes={
+        'input': {
+            0: 'batch',
+        },
+        'simcc_x': {
+            0: 'batch'
+        },
+        'simcc_y': {
+            0: 'batch'
+        },
+        'simcc_z': {
+            0: 'batch'
+        }
+    })
